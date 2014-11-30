ac58d86b982a54e11de3a9a23f77f7cde2fd02a1
Emory Merryman <emory.merryman@gmail.com>
2014-11-30 10:18:06 -0500

    simplified par.sh while changing document

--- par.sh
+++ par.sh
@@ -2,11 +2,11 @@
 
 WORK_DIR=$(mktemp -d)
 
-mkdir ${WORK_DIR}/classes
+TSTAMP=$(date +%Y-%m-%d-%H-%M)
 
-javac -classpath jbpm-jpdl-3.2.3.jar:${HOME}/openkm-6.3.0-community.backup/tomcat/webapps/OpenKM/WEB-INF/classes -d ${WORK_DIR}/classes ${@}/src/peasanteastern/*.java
+mkdir ${WORK_DIR}/classes
 
 cp ${@}/processdefinition.xml ${WORK_DIR}
 cp ${@}/forms.xml ${WORK_DIR}
 
-jar -cf ${@}.par -C ${WORK_DIR} .
+jar -cf ${@}.${TSTAMP}.par -C ${WORK_DIR} .
