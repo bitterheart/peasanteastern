0e0a2f19b238f3144098d040684bd095a2eb4b3f
Emory Merryman <emory.merryman@gmail.com>
2014-11-30 19:04:03 -0500

    I think it works

--- archive_scans.sh
+++ archive_scans.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SCAN_DIR=/run/media/Yc6hR2VEpBdiFT/D513-FB22/EPSCAN/001
+SCAN_DIR=/tmp/tmp.RhHPE8diZb
 
 if [ -d ${SCAN_DIR} ]
 then
@@ -10,8 +11,10 @@ then
     fi
     for FILE in ${SCAN_DIR}/*.JPG
     do
+        echo BEGIN ${FILE}
         while [ -f ${FILE} ] && [[ $(lsof | grep ${FILE}) ]]
         do
+            echo SLEEP A ${FILE}
             sleep 1s
         done
         if [ -f ${FILE} ] && ! [[ $(lsof | grep ${FILE}) ]]
@@ -20,6 +23,7 @@ then
                 flock --exclusive 9
                 TEMPFILE=$(mktemp /tmp/$(basename ${FILE}).XXXXXXXXXXXXXXX.PDF)
                 NEWFILE=$(mktemp ${HOME}/temp/scans/$(basename ${FILE}).XXXXXXXXXXXXXX.PDF)
+                echo ${FILE} convert ${NEWFILE}
                 convert ${FILE} ${TEMPFILE}
                 mv ${TEMPFILE} ${NEWFILE}
                 rm ${FILE}
@@ -27,5 +31,5 @@ then
             rm ${FILE}.lock
         fi
     done
-    sleep 1s
+#    umount ${SCAN_DIR}
 fi
